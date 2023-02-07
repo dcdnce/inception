4ce517829756838c1ca261c1a713f0a86b491364
pforesti <pforesti@student.42nice.fr>
2023-02-07 07:25:31 -0500

    added zsh to script

--- 2_asuser.sh
+++ 2_asuser.sh
@@ -7,3 +7,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo -e "\e[32mConfiguring git credentials\e[0m"
 git config --global user.email "pforesti@student.42nice.fr"
 git config --global user.name "pforesti"
+
+echo -e "\e[32mInstalling zsh & oh-my-zsh...\e[0m"
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
