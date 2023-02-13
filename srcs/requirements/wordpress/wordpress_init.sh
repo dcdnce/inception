2dc0483460aa7247b59f663d5bf46a1964a646ef
pforesti <pforesti@student.42nice.fr>
2023-02-13 09:29:35 -0500

    advancing on wordpress

--- srcs/requirements/wordpress/wordpress_init.sh
+++ srcs/requirements/wordpress/wordpress_init.sh
@@ -1,13 +1,16 @@
-# Download wp-cli & path it as "wp"
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp 
+if [ ! -f "wp-config.php" ]; then
+	service mysql start
 
-if [ ! -f "wp-config.php" ]
-then
+	# Download wp-cli & path it as "wp"
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp 
 
-# Download and install wordpress
-wp core download --allow-root
-wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN --dbpass=$DB_PASS --allow-root
-
+	# Download and install wordpress
+	wp core download --allow-root
+	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN --dbpass=$DB_PASS --allow-root
+	wp core install --url=https://$WP_URL/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
 fi
+
+# Launch php-fpm -root -foreground
+#php-fpm -RF
