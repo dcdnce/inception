5a4f5c5a8b110a7e95c845defa167c7051cd11c4
pforesti <pforesti@student.42nice.fr>
2023-02-13 12:55:31 -0500

    added www.conf trying to make php-fpm work now

--- srcs/requirements/wordpress/wordpress_init.sh
+++ srcs/requirements/wordpress/wordpress_init.sh
@@ -8,7 +8,8 @@ if [ ! -f "wp-config.php" ]; then
 	wp core download --allow-root
 	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN --dbpass=$DB_PASS --allow-root
 	wp core install --url=https://$WP_URL/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
+
 fi
 
 # Launch php-fpm -root -foreground
-#php-fpm -RF
+php-fpm7.3 -RF
